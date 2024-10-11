e8b35703ec41cdba695ef37815f0dba9a840d404
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-12 01:21:12 +0530

    Push Card Project

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind tst</h1>
       <Card username="ArjunKumar" btnText="Click me"/>
-      <Card username="Itagi" />
+      <Card username="Kumar" />
     </>
-
   )
 }
 
