5c29ad7fe6f79d655c4037325b00ff596013dad9
arjunkumar811 <kumararjun26401@gmail.com>
2024-10-12 01:10:08 +0530

    Added components folder

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -6,8 +6,11 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind tst</h1>
+      <Card username="ArjunKumar" btnText="Click me"/>
+      <Card username="Itagi" />
     </>
+
   )
 }
 
